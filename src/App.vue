617358ae601b737d461d7dc7fcea840a74395806
ChenYijun <3098517519@qq.com>
2026-07-19 13:52:18 +0800

    添加element-plus组件库，实现仓库持久化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from './stores/user'
+const userStore = useUserStore()
+</script>
 
 <template>
   <div>app</div>
+  <p></p>
+  <div>{{ userStore.token }}</div>
+  <el-button @click="userStore.setToken('asjhdiohewoiasdw')" type="">更新</el-button>
+  <el-button @click="userStore.removeToken()" type="">退出</el-button>
 </template>
 
 <style scoped></style>
