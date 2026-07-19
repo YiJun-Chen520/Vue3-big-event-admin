d3a379d94774ec95029bba65caa83f28fda15438
ChenYijun <3098517519@qq.com>
2026-07-19 16:21:03 +0800

    实现注册登录的表单校验

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
-<script setup>
-import { useUserStore } from './stores/index'
-const userStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
-  <router-view></router-view>
-  <hr />
-  <hr />
-  <hr />
-  <div>app</div>
-  <p></p>
-  <div>{{ userStore.token }}</div>
-  <el-button @click="userStore.setToken('asjhdiohewoiasdw')" type="">更新</el-button>
-  <el-button @click="userStore.removeToken()" type="">退出</el-button>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style scoped></style>
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,7 +1,149 @@
-<template>登录注册</template>
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { reactive, ref } from 'vue'
 
-<script>
-export default {}
+const isRegister = ref(true)
+
+const formModel = reactive({
+  username: '',
+  password: '',
+  repassword: '',
+})
+
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' },
+    { pattern: /^\S{6,12}$/, message: '密码必须在 6 到 12 个字符之间', trigger: 'blur' },
+  ],
+  repassword: [
+    {
+      validator: (rule, value, callback) => {
+        if (value !== formModel.password) {
+          callback(new Error('两次输入的密码不一致'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur',
+    },
+  ],
+}
 </script>
 
-<style></style>
+<template>
+  <!-- er-row 一行24份
+      er-col 表示一列
+      er-offset 表示偏移
+      el-form 表单
+      el-form-item 表单项
+  -->
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <el-form
+        :model="formModel"
+        :rules="rules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input
+            v-model="formModel.repassword"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请再次输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space> 注册 </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false"> ← 返回 </el-link>
+        </el-form-item>
+      </el-form>
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>登录</el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true"> 注册 → </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+    .title {
+      margin: 0 auto;
+    }
+    .button {
+      width: 100%;
+    }
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
